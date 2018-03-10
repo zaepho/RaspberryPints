da8598aa4728613b5f7be6e92e17265ccbd708c6
zaepho <zaepho@users.noreply.github.com>
2018-03-09 19:41:04 -0600

    junk

--- includes/common.php
+++ includes/common.php
@@ -1,5 +1,5 @@
 <?php
-$RPintsVersion = '2.0.3dev';
+$RPintsVersion = '2.0.3-dev';
 require __DIR__ . '/../vendor/autoload.php';
 # require_once __DIR__ . '/../vendor/apache/log4php/src/main/php/Logger.php';
 
